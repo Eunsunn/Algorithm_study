1985349442ef855892ab75fc156c74bf591de9aa
eunsun <dmstjs8808@gmail.com>
2020-03-23 15:53:25 +0900

    brute force

--- wk10_baekjoon6603.java
+++ wk10_baekjoon6603.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main
 {
-	//문제: https://www.acmicpc.net/problem/6603 (백준 6603 로또)
+	//문제: 백준 로또
 	public static int k;
 	public static int[] num;
 	public static final int select = 6;
@@ -21,7 +20,6 @@ public class Main
 			num = new int[k];
 			for(int i=0; i<k; i++)
 				num[i] = Integer.parseInt(st.nextToken());
-			Arrays.sort(num);
 			makeSet("", -1, 0);
 			System.out.println();	
 		}
@@ -29,10 +27,14 @@ public class Main
 	}
 	
 	public static void makeSet(String out, int endIdx, int count) {
-		if(count==select) 
-			System.out.println(out);
-		for(int i=endIdx+1; i<k; i++)
-			makeSet(out+num[i]+" ", i, count+1);
+		if(count>select) return;
+        if(count==select) 
+        {
+            System.out.println(out);
+            return;
+        }
+        for(int i=endIdx+1; i<k; i++)
+            makeSet(out+num[i]+" ", i, count+1);   
 	}
 
 }
